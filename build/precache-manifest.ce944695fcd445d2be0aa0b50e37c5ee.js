4aafc81525717182d91d73919f73b8f8f78b1f37
POOJAPRABHU450 <64356490+POOJAPRABHU450@users.noreply.github.com>
2020-09-08 13:37:04 +0530

    Add files via upload

---
+++ build/precache-manifest.ce944695fcd445d2be0aa0b50e37c5ee.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a83a7e43c440fd038248a012b5d06612",
+    "url": "/index.html"
+  },
+  {
+    "revision": "ad138d74434e76e4bd3f",
+    "url": "/static/css/main.b9539ae8.chunk.css"
+  },
+  {
+    "revision": "4e4d35ba3784060b8de3",
+    "url": "/static/js/2.a73874cb.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.a73874cb.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "ad138d74434e76e4bd3f",
+    "url": "/static/js/main.eb783e98.chunk.js"
+  },
+  {
+    "revision": "419d296c21600b90fe58",
+    "url": "/static/js/runtime-main.dfac0e00.js"
+  }
+]);
---
+++ src/index.js
@@ -0,0 +1,26 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import './index.css';
+import SearchMovies from './searchMovie'
+
+
+
+
+class Main extends React.Component {
+  render() {
+    return (
+      <div className="container">
+        <h1 className='title'>Movie Search app</h1>
+        <SearchMovies />
+      </div>
+    );
+  }
+}
+
+ReactDOM.render(<Main />, document.getElementById('root'));
+
+
+
+
+
+
